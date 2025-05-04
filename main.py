363292a2d4382fdf8aaf39b11f185460cee2b4bd
mcelrjo <mcelroy@auburn.edu>
2025-05-04 12:03:01 -0500

    Add CORS middleware fro Netlify frontend

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI
-from app.api import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="TurfAI Backend",
-    description="API for Turfgrass GenAI Assistant",
+    description="Lawncare AI Assistant",
     version="0.1"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://greenai.netlify.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
+
